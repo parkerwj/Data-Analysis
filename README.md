707cda9535243d496eb058d0a28254fac1154928
Parker Johnson <johnsonpar@byui.edu>
2023-10-28 12:34:49 -0600

    adding to github

---
+++ README.md
@@ -0,0 +1,62 @@
+# Overview
+
+This Python script leverages the `yfinance` library to fetch historical stock data from Yahoo Finance, analyze it with basic and advanced metrics, and create insightful visualizations. The analysis includes computing indicators like moving averages and the Relative Strength Index (RSI). Users can interactively input a stock symbol and the number of years for data retrieval, and the script dynamically generates answers to predefined questions about the stock's performance, with real-time data. It's a versatile tool for software engineers seeking hands-on experience in financial data analysis and visualization.
+
+## Data Set Description
+
+I analyzed historical stock data, and the data was obtained from Yahoo Finance using the [yfinance library](https://pypi.org/project/yfinance/).
+
+## Purpose
+
+The purpose of this software is to analyze historical stock data, answer specific questions about the stock's performance, and visualize the results. By doing this, I aim to enhance my skills as a software engineer in data analysis and data visualization within the financial domain.
+
+## Software Demo Video
+
+You can watch a 4-5 minute demonstration of this project in action on [YouTube](http://youtube.link.goes.here).
+
+# Data Analysis Results
+
+The responses to these questions dynamically adjust as the data is sourced live from Yahoo Finance. The code includes variables that will generate and display the answers when executed
+
+- **Company Name**: [Company Name]
+- **Question 1**: What is the historical stock price data for [Company Name] for the past [years] years?
+  [Historical stock price data]
+
+- **Question 2**: What is the 50-day simple moving average (SMA) of [Company Name]'s stock price?
+  [50-Day SMA data]
+
+- **Question 3**: What is the daily returns of [Company Name]'s stock during the specified date range?
+  [Daily Returns data]
+
+- **Question 4**: What is the 14-day relative strength index (RSI) for [Company Name] ([Stock Symbol])?
+  [14-Day RSI data]
+
+- **Question 5**: Are there any overbought or oversold conditions for [Company Name] ([Stock Symbol]) based on RSI?
+  The code plots horizontal lines at RSI values of 30 (indicating oversold conditions) and 70 (indicating overbought conditions) on the RSI chart.
+
+# Development Environment
+
+I developed this software using the following tools and programming languages:
+
+- Python 3.11.5
+- [yfinance](https://pypi.org/project/yfinance/) library
+- [matplotlib](https://matplotlib.org/) for data visualization
+- [ta](https://technical-analysis-library-in-python.readthedocs.io/en/latest/) library for calculating technical indicators
+- [datetime](https://docs.python.org/3/library/datetime.html) for handling date and time data
+
+# Useful Websites
+
+I found the following websites helpful in this project:
+
+- [Yahoo Finance](https://finance.yahoo.com/)
+- [yfinance Python library documentation](https://pypi.org/project/yfinance/)
+- [Matplotlib Documentation](https://matplotlib.org/stable/contents.html)
+
+# Future Work
+
+In the future, I plan to work on the following improvements and additions to this project:
+
+- Item 1: Add more technical indicators and analysis methods.
+- Item 2: Enhance data visualization by creating interactive plots.
+- Item 3: Implement a user-friendly interface for input and interaction.
+
---
+++ stock_analysis.py
@@ -0,0 +1,99 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+import ta
+from datetime import datetime, timedelta
+
+def fetch_stock_data(stock_symbol, years):
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=years * 365)
+
+    data = yf.download(stock_symbol, start=start_date, end=end_date)
+    return data
+
+def get_company_name(stock_symbol):
+    try:
+        stock_info = yf.Ticker(stock_symbol)
+        return stock_info.info['longName']
+    except:
+        return stock_symbol
+
+def basic_stock_analysis(data):
+    data['Daily Returns'] = data['Adj Close'].pct_change()
+    data['50-Day SMA'] = data['Adj Close'].rolling(window=50).mean()
+    data['50-Day STD'] = data['Adj Close'].rolling(window=50).std()
+    return data
+
+def advanced_stock_analysis(data):
+    data['200-Day SMA'] = data['Adj Close'].rolling(window=200).mean()
+    data['Upper Bollinger Band'] = data['50-Day SMA'] + 2 * data['50-Day STD']
+    data['Lower Bollinger Band'] = data['50-Day SMA'] - 2 * data['50-Day STD']
+
+    # Calculate 14-Day RSI using ta library
+    data['RSI'] = ta.momentum.RSIIndicator(data['Adj Close'], window=14).rsi()
+
+    return data
+
+def plot_stock_data(data, stock_name):
+    plt.figure(figsize=(12, 10))
+
+    plt.subplot(3, 1, 1)
+    plt.plot(data.index, data['Adj Close'], label='Stock Price', color='blue')
+    plt.plot(data.index, data['50-Day SMA'], label='50-Day SMA', color='orange')
+    plt.fill_between(data.index, data['50-Day SMA'] - 2 * data['50-Day STD'],
+                     data['50-Day SMA'] + 2 * data['50-Day STD'], color='lightgray', alpha=0.5)
+    plt.title(f'Stock Price and 50-Day Simple Moving Average ({stock_name})')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.legend()
+
+    plt.subplot(3, 1, 2)
+    plt.plot(data.index, data['Daily Returns'], color='red')
+    plt.axhline(0, color='black', linestyle='--')
+    plt.title(f'Daily Returns ({stock_name})')
+    plt.xlabel('Date')
+    plt.ylabel('Returns')
+
+    plt.subplot(3, 1, 3)
+    plt.plot(data.index, data['RSI'], label='14-Day RSI', color='green')
+    plt.axhline(30, color='red', linestyle='--', label='Oversold (30)')
+    plt.axhline(70, color='blue', linestyle='--', label='Overbought (70)')
+    plt.title(f'14-Day RSI ({stock_name})')
+    plt.xlabel('Date')
+    plt.ylabel('RSI')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+def main():
+    stock_symbol = input("Enter the stock symbol: ")
+    years = int(input("Enter the number of years for historical data retrieval: "))
+
+    data = fetch_stock_data(stock_symbol, years)
+    data = basic_stock_analysis(data)
+    data = advanced_stock_analysis(data)
+
+    stock_name = get_company_name(stock_symbol)
+
+    # Print questions and answers
+    print(f"Company Name: {stock_name}")
+    print(f"Question 1: What is the historical stock price data for {stock_name} for the past {years} years?")
+    print(data)
+
+    print(f"Question 2: What is the 50-day simple moving average (SMA) of {stock_name}'s stock price?")
+    print(data['50-Day SMA'])
+
+    print(f"Question 3: What is the daily returns of {stock_name}'s stock during the specified date range?")
+    print(data['Daily Returns'])
+
+    print(f"Question 4: What is the 14-day relative strength index (RSI) for {stock_name} ({stock_symbol})?")
+    print(data['RSI'])
+
+    print(f"Question 5: Are there any overbought or oversold conditions for {stock_name} ({stock_symbol}) based on RSI?")
+    print("The code plots horizontal lines at RSI values of 30 (indicating oversold conditions) and 70 (indicating overbought conditions) on the RSI chart.")
+
+    # Plot the stock data with the stock name in the title
+    plot_stock_data(data, stock_name)
+
+if __name__ == "__main__":
+    main()
